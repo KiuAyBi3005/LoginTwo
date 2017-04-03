a03e15e59e5e48be62d17304154f9930cc57813d
Quang Anh <quanganhpham300596@gmail.com>
2017-04-03 09:21:22 +0700

    Update commit

--- app/src/main/java/com/example/quanganhpham/logintwo/MainActivity.java
+++ app/src/main/java/com/example/quanganhpham/logintwo/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_new_account);
+        setContentView(R.layout.activity_create);
     }
 }
